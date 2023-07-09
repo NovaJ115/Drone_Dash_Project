93ab45eb66072afe099fef40ad5b990edf44cf2b
NovaJ115 <111328457+NovaJ115@users.noreply.github.com>
2023-07-08 23:42:14 -0500

    Resume Countdown
    Added a countdown after a question to give the player time before being flown back into the game

--- Assets/Scripts/QuestionButtons.cs
+++ Assets/Scripts/QuestionButtons.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionButtons : MonoBehaviour
 {
     public QuestionManager questionManager;
-    public void ButtonPress()
+    public bool incorrect;
+    public bool correct;
+    public void ButtonPressed()
     {
-        Time.timeScale = 1;
-        questionManager.questionMenu.SetActive(false);
+        if(correct)
+        {
+            questionManager.TheCorrectAnswer();
+            StartCoroutine(AnswerCorrect());
+        }
+        if (incorrect)
+        {
+            StartCoroutine(AnswerIncorrect());
+        }
         
+    }
+
+    public IEnumerator AnswerCorrect()
+    {
+        this.GetComponent<Image>().color = Color.green;
+        yield return new WaitForSecondsRealtime(1.99f);
+        this.GetComponent<Image>().color = Color.white;
+        
+    }
+    public IEnumerator AnswerIncorrect()
+    {
+        this.GetComponent<Image>().color = Color.red;
+
+
+        yield return new WaitForSecondsRealtime(2);
+        Time.timeScale = 1;
+        this.GetComponent<Image>().color = Color.white;
+        questionManager.questionMenu.SetActive(false);
     }
 
 }
--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -1,16 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class QuestionManager : MonoBehaviour
 {
     public GameObject questionMenu;
 
+    public GameObject resumeTimerScreen;
+    public TextMeshProUGUI timerText;
+    public float timeAmount;
+
+    public bool countdownActive;
+
+    public float time;
+
+    public void Start()
+    {
+        countdownActive = false;
+        time = timeAmount;
+    }
     public void OpenQuestionMenu()
     {
         questionMenu.SetActive(true);
         Time.timeScale = 0;
+
+    }
+    public void ResumeCountdown()
+    {
+        resumeTimerScreen.SetActive(true);
+        
     }
 
+
+    public void TheCorrectAnswer()
+    {
+        StartCoroutine(CorrectAnswer());
+    }
+
+    public IEnumerator CorrectAnswer()
+    {
+        
+        yield return new WaitForSecondsRealtime(2);
+        
+        questionMenu.SetActive(false);
+        resumeTimerScreen.SetActive(true);
+        timerText.text = "Resume in: 3";
+        yield return new WaitForSecondsRealtime(1);
+        timerText.text = "Resume in: 2";
+        yield return new WaitForSecondsRealtime(1);
+        timerText.text = "Resume in: 1";
+        yield return new WaitForSecondsRealtime(1);
+        resumeTimerScreen.SetActive(false);
+        Time.timeScale = 1;
+        Debug.Log("Coroutine Finished");
+    }
 
 }
